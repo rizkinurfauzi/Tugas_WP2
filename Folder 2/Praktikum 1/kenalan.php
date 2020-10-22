993135a040d714b5eee0fc107d2efc97eade6b4c
rizkinurfauzi <rizkinurfauzi0@gmail.com>
2020-10-22 19:00:37 +0700

    Rizki Nur Fauzi 18090054

---
+++ Folder 2/Praktikum 1/kenalan.php
@@ -0,0 +1,10 @@
+<?php
+class kenalan extends CI_Controller{
+    public function index() {
+        echo "<h1> perkenalan</h1>";
+        echo "Rizki Nur Fauzi<br>
+                saya tinggal di Kaligangsa Wetan<br>
+                saya suka Bermain game";
+    }
+
+}
